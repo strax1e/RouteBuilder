9de9bfcdbb001f509bd7f3c6692c8b75db122d07
V7074 <waiddew@gmail.com>
2021-07-10 20:02:22 +0300

    GUI added

--- app/src/main/java/com/gvm/routebuilder/MainActivity.kt
+++ app/src/main/java/com/gvm/routebuilder/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.gvm.routebuilder
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.gvm.routebuilder.database.entities.Road
 import com.gvm.routebuilder.viewmodel.MainViewModel
+import kotlin.random.Random
 
 /**
  * MainActivity class makes the main activity of the application work
@@ -127,10 +133,85 @@ class MainActivity : AppCompatActivity() {
         this.changeStepButtonsState(!isStart)
     }
 
+    private val mapIdToImageButton = HashMap<Short, ImageButton>()
+
+    private fun createNodes() {
+        val setOfPairXY = HashSet<Pair<Float, Float>>()
+        mainVM.ldTownsAndRoads.value!!.second.forEach {
+
+            val setAttrsButton = fun(button: ImageButton, coordinates: Pair<Float, Float>) {
+                button.apply {
+                    visibility = Button.VISIBLE
+                    x = coordinates.first
+                    y = coordinates.second
+                    setBackgroundResource(R.drawable.roundedbutton)
+                }
+            }
+            var imageView = findViewById<ImageView>(R.id.image)
+            val createButton = fun(node: Short) {
+                var coordinates = Pair<Float, Float>(0f, 0f)
+                if (!mapIdToImageButton.contains(node)) {
+                    do {
+                        coordinates = Pair(Random.nextFloat() * imageView.width, Random.nextFloat() * imageView.height)
+                    } while (setOfPairXY.contains(coordinates))
+                    setOfPairXY.add(coordinates)
+                    val button = ImageButton(this)
+                    setAttrsButton(button, coordinates)
+                    mapIdToImageButton[node] = button
+                }
+            }
+            createButton(it.nodeA)
+            createButton(it.nodeB)
+        }
+    }
+
+    fun deleteNodes(buttons: Collection<ImageButton>) {
+        val layout = findViewById<ConstraintLayout>(R.id.field)
+        for (button in mapIdToImageButton.values) {
+            layout.removeView(button)
+        }
+    }
+
+    private fun renderNodes() {
+        val layout = findViewById<ConstraintLayout>(R.id.field)
+        val lp = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.WRAP_CONTENT,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        for (button in mapIdToImageButton.values) {
+            layout.addView(button, lp)
+        }
+    }
+
+    private fun renderEdges() {
+        var imageView = findViewById<ImageView>(R.id.image)
+        val paint = Paint()
+        val bitmap = Bitmap.createBitmap(imageView.width, imageView.height, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(bitmap)
+
+        paint.apply {
+            color = Color.WHITE
+            strokeWidth = 10f
+        }
+
+        mainVM.ldTownsAndRoads.value!!.second.forEach {
+            val buttonA = mapIdToImageButton[it.nodeA]
+            val buttonB = mapIdToImageButton[it.nodeB]
+            canvas.drawLine(buttonA!!.x, buttonA.y, buttonB!!.x, buttonB.y, paint)
+        }
+
+        imageView.setImageBitmap(bitmap)
+    }
+
     // MainViewModel declaration
     private val mainVM by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
 
     private val townAndRoadsObserver = Observer<Pair<Map<Short, String>, Collection<Road>>> {
+        createNodes()
+        renderEdges()
+        renderNodes()
         switchStopStartButton(true)
         startButton.isEnabled = true
     }
